12cab0ab1835f7ca1b61cea3134e3cf03c180f1a
Ligia Brusamolin <librusamolin@gmail.com>
2021-04-07 15:59:27 -0300

    Correção de membros na página de Lazer.

--- src/views/Lazer.vue
+++ src/views/Lazer.vue
@@ -102,6 +102,55 @@
           </v-card-actions>
         </v-card>
       </v-flex>
+      <v-flex xs12 md6>
+        <v-card class="card-area" elevation="20">
+          <v-row class="avatars">
+            <v-avatar size="150">
+              <img
+                src="../../assets/images/lazer/foto_luiz_adriano.jpg"
+                alt="Luiz Adriano"
+              />
+            </v-avatar>
+            <img
+              style="width: 200px; height: 116px"
+              class="logo"
+              src="../../assets/images/lazer/folivora_logo.png"
+              alt="Logo Folivora Gin"
+            />
+          </v-row>
+          <v-card-title>Luiz Adriano</v-card-title>
+          <v-card-subtitle
+            ><strong>Folivora Gin</strong> | Produtos Alimentícios -
+            Bebidas</v-card-subtitle
+          >
+          <v-card-text
+            >Destilaria exclusiva de GIN situado na serra dos órgãos na cidade
+            de Teresópolis aonde destilamos o FOLIVORA GIN. Atendemos a todo o
+            Brasil no Atacado ou Varejo.
+          </v-card-text>
+          <v-card-actions>
+            <v-btn text
+              ><a href="https://api.whatsapp.com/send?phone=5521993368570"
+                ><v-icon>mdi-whatsapp</v-icon> +55 21 99336-8570</a
+              ></v-btn
+            >
+          </v-card-actions>
+          <v-card-actions>
+            <v-btn text
+              ><a href="mailto:‎luizadrianorj@gmail.com"
+                >‎<v-icon>mdi-email-outline</v-icon> luizadrianorj@gmail.com</a
+              ></v-btn
+            >
+          </v-card-actions>
+          <v-card-actions>
+            <v-btn text
+              ><a href="https://www.folivoragin.com/"
+                >‎<v-icon>mdi-earth</v-icon> folivoragin.com</a
+              ></v-btn
+            >
+          </v-card-actions>
+        </v-card>
+      </v-flex>
     </v-layout>
   </v-container>
 </template>
