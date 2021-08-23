587a046c6c930db0390605361ddeec467005be50
Ligia Rezende - LRE <ligia.rezende@iteris.com.br>
2021-08-23 18:15:33 -0300

    Exclusão de membro na página de Tecnologia.

--- src/views/Tecnologia.vue
+++ src/views/Tecnologia.vue
@@ -56,56 +56,6 @@
           </v-card-actions>
         </v-card>
       </v-flex>
-      <v-flex xs12 md6>
-        <v-card class="card-area" elevation="20">
-          <v-row class="avatars">
-            <v-avatar size="150">
-              <img
-                src="../../assets/images/tecnologia/foto_ligia_brusamolin.jpg"
-                alt="Ligia Brusamolin"
-              />
-            </v-avatar>
-            <img
-              style="width: 200px; height: 79px"
-              class="logo"
-              src="../../assets/images/tecnologia/logo-molin-space.png"
-              alt="Logo Molin Space Desenvolvimento Web"
-            />
-          </v-row>
-          <v-card-title>Ligia Brusamolin</v-card-title>
-          <v-card-subtitle
-            ><strong>Molin Space: Desenvolvimento Web</strong> | Desenvolvimento
-            de Sites e E-commerce</v-card-subtitle
-          >
-          <v-card-text
-            >Agência de Desenvolvimento Web & Mobile. Desenvolvemos, fazemos a
-            manutenção e otimizamos: sites, lojas virtuais, landing pages, blogs
-            e aplicativos. Também temos nosso próprio produto: um sistema
-            delivery para bares e restaurantes.
-          </v-card-text>
-          <v-card-actions>
-            <v-btn text
-              ><a href="https://api.whatsapp.com/send?phone=5521984418877"
-                ><v-icon>mdi-whatsapp</v-icon> +55 21 98441-8877</a
-              ></v-btn
-            >
-          </v-card-actions>
-          <v-card-actions>
-            <v-btn text wrap
-              ><a href="mailto:‎ligia@molin.space"
-                >‎<v-icon>mdi-email-outline</v-icon> ligia@molin.space</a
-              ></v-btn
-            >
-          </v-card-actions>
-          <v-card-actions>
-            <v-btn text
-              ><a href="https://molin.space/"
-                >‎<v-icon>mdi-earth</v-icon> molin.space</a
-              ></v-btn
-            >
-          </v-card-actions>
-        </v-card>
-      </v-flex>
     </v-layout>
   </v-container>
 </template>
